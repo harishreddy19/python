feb9b3249498aa450a78728fe4f8908a66d21b7c
harishreddy19 <138599831+harishreddy19@users.noreply.github.com>
2023-10-20 14:07:21 +0530

    Create addition.py
    here we adding two numbers by taking input from the user

---
+++ addition.py
@@ -0,0 +1,5 @@
+#addition of two numbers using python 
+a=int(input("enter any number"))  # WE ARE TAKING INPUT FROM USER
+b=int(input("enter second number"))
+sum=a+b
+print(" THE ADDITION OF TWO NUMBERS IS :",sum)
