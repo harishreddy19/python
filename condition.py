0f369d43f08a00f93480dd7db6752db5c39913be
harishreddy19 <138599831+harishreddy19@users.noreply.github.com>
2023-10-20 14:17:39 +0530

    Create condition.py
    there are four conditional statements  in python :
    1) SIMPLE IF
    2)IF .....ELSE
    3)CHAINED IF 
    4) IF...ELIF..IF
    5)CHAINED IF
    THANK TOU

---
+++ condition.py
@@ -0,0 +1,7 @@
+#conditional statements
+#program to check a person  is eligible to vote
+n=int(input("pls enter your age")
+if n>=18:
+   print("yes your are eligible for vote",n)
+else:
+   print("no you are not eligible for vote",n)
